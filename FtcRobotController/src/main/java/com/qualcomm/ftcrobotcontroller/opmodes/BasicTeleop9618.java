48ea78599683b02a519ae948f9f12f6cdfe85281
Cyber Spartans FTC <cyberspartans9618@gmail.com>
2016-02-24 14:17:04 -0500

    changes made by jared
    i don't know what these were supposed to be? maybe he knew what he was
    doing, maybe he didn't

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicTeleop9618.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicTeleop9618.java
@@ -1,10 +1,8 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
 
 /**
  * Created by 000066hs on 12/1/2015.
@@ -12,17 +10,12 @@ import com.qualcomm.robotcore.util.Range;
 
 
 /*
-
-so, as far as i can tell the only issue we have now is that android studio won't generate an APK. it keeps saying it's building the project but there's never an apk folder... ???
-
-
-
-
-
+so, as far as i can tell the only issue we have now is that android studio won't generate an APK.
+It keeps saying it's building the project but there's never an apk folder... ???
  */
 
-
 public class BasicTeleop9618 extends OpMode {
+
 /*
     // TETRIX VALUES.
     final static double ARM_MIN_RANGE  = 0.20;
@@ -41,11 +34,12 @@ public class BasicTeleop9618 extends OpMode {
 
     // amount to change the claw servo position by
     double clawDelta = 0.1;
-*/
+    */
+
     DcMotor motorRight;
     DcMotor motorLeft;
-    //Servo claw;
-    //Servo arm;
+  //  DcMotor armPosition1;
+
 
     /**
      * Constructor
@@ -79,17 +73,16 @@ public class BasicTeleop9618 extends OpMode {
 		 *    "servo_1" controls the arm joint of the manipulator.
 		 *    "servo_6" controls the claw joint of the manipulator.
 		 */
-        motorRight = hardwareMap.dcMotor.get("motor_2");
-        motorLeft = hardwareMap.dcMotor.get("motor_1");
+        motorRight = hardwareMap.dcMotor.get("motor_1");
+        motorLeft = hardwareMap.dcMotor.get("motor_0");
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-        /*
-        //arm = hardwareMap.servo.get("servo_1");
-        //claw = hardwareMap.servo.get("servo_6");
+      //  armPosition1 = hardwareMap.dcMotor.get("servo_1");
+
 
         // assign the starting position of the wrist and claw
-        armPosition = 0.2;
-        clawPosition = 0.2;
-        */
+        //armPosition = 0.2;
+        //clawPosition = 0.2;
+
     }
 
     /*
@@ -111,29 +104,54 @@ public class BasicTeleop9618 extends OpMode {
         // 1 is full down
         // direction: left_stick_x ranges from -1 to 1, where -1 is full left
         // and 1 is full right
-        float throttle = -gamepad1.left_stick_y;
-        float direction = gamepad1.left_stick_x;
-        float right = throttle - direction;
-        float left = throttle + direction;
+        float throttle = gamepad1.left_stick_y;
+        float direction = gamepad1.right_stick_y;
+        float left = throttle;
+        float right = direction;
+        //float armMovement1 = gamepad1.right_stick_y;
         telemetry.addData("right motor power", right);
         telemetry.addData("left motor power", left);
+        //telemetry.addData("climber saver", armMovement1)
 
 
         // clip the right/left values so that the values never exceed +/- 1
-        right = Range.clip(right, -1, 1);
-        left = Range.clip(left, -1, 1);
+          // right = Range.clip(right, -1, 1);             //ABANDONED
+          // left = Range.clip(left, -1, 1);
 
         // scale the joystick value to make it easier to control
-        // the robot more precisely at slower speeds.
-        right = (float)scaleInput(right);
-        left =  (float)scaleInput(left);
+        //  the robot more precisely at slower speeds.                   //DONT FORGET
+         // right = (float)scaleInput(right);                            //ABANDONED
+       // left =  (float)scaleInput(left);
 
-        // write the values to the motors
-        motorRight.setPower(right);
-        motorLeft.setPower(left);
-        /*
+        // write the values to the motors and make sure values don't exceed '1'
+       if (right > 1){
+           right = 1;
+           motorRight.setPower(right);
+           motorLeft.setPower(left);
+       }
+        else if(right < -1){
+           right = -1;
+           motorRight.setPower(right);
+           motorLeft.setPower(left);
+       }
+        else if(left > 1){
+           left = 1;
+           motorRight.setPower(right);
+           motorLeft.setPower(left);
+       }
+        else if(left < -1){
+           left = 1;
+           motorRight.setPower(right);
+           motorLeft.setPower(left);
+       }
+        else{
+           motorRight.setPower(right);
+           motorLeft.setPower(left);
+       }
+       //  armPosition1.setPower(armMovement1);
+
         // update the position of the arm.
-        if (gamepad1.a) {
+        /*if (gamepad1.a) {
             // if the A button is pushed on gamepad1, increment the position of
             // the arm servo.
             armPosition += armDelta;
@@ -159,22 +177,22 @@ public class BasicTeleop9618 extends OpMode {
         clawPosition = Range.clip(clawPosition, CLAW_MIN_RANGE, CLAW_MAX_RANGE);
 
         // write position values to the wrist and claw servo
-        arm.setPosition(armPosition);
-        claw.setPosition(clawPosition);
+        //arm.setPosition(armPosition);
+        //claw.setPosition(clawPosition);
 
-        */
 
-		/*
+
+
 		 * Send telemetry data back to driver station. Note that if we are using
 		 * a legacy NXT-compatible motor controller, then the getPower() method
 		 * will return a null value. The legacy NXT-compatible motor controllers
 		 * are currently write only.
 		 */
-        //telemetry.addData("Text", "*** Robot Data***");
-        //telemetry.addData("arm", "arm:  " + String.format("%.2f", armPosition));
+     //   telemetry.addData("Text", "*** Robot Data***");
+       // telemetry.addData("arm", "arm:  " + String.format("%.2f", armPosition1));
         //telemetry.addData("claw", "claw:  " + String.format("%.2f", clawPosition));
-        //telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
-        //telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
+       // telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
+       // telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
 
     }
 
@@ -189,17 +207,16 @@ public class BasicTeleop9618 extends OpMode {
     }
 
 
-    /*
+    /*            ABANDONED
      * This method scales the joystick input so for low joystick values, the
      * scaled value is less than linear.  This is to make it easier to drive
      * the robot more precisely at slower speeds.
      */
-    double scaleInput(double dVal)  {
-        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+   /* double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24, 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
 
         // get the corresponding index for the scaleInput array.
-        int index = (int) (dVal * 16.0);
+        int index = (int) (dVal * 16);
 
         // index should be positive.
         if (index < 0) {
@@ -220,9 +237,9 @@ public class BasicTeleop9618 extends OpMode {
         }
 
         // return scaled value.
-        return dScale;
+        return dScale*20;
     }
-
+*/
 
 
 
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CVAuton9618.java
@@ -0,0 +1,33 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by 000066hs on 12/2/2015.
+ * thingy to do the thingy with auton mode with openCV
+ *
+ *
+ */
+public class CVAuton9618 extends OpMode {
+
+    @Override
+    public void init() {
+        /*Initializing hardware
+        motorRight = hardwareMap.dcMotor.get("motor_2");
+        motorLeft = hardwareMap.dcMotor.get("motor_1");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+*/
+
+    }
+
+    @Override
+    public void loop() {
+
+
+
+
+    }
+
+}
